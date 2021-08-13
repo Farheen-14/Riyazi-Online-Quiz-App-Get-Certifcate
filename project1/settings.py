d6c73e0935f23be1406774a5c5d446d8b89446c2
Farheen <farheenriyazi14@gmail.com>
2021-08-13 16:56:33 +0530

    riyazi-quiz hosting and  testing

--- project1/settings.py
+++ project1/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,10 +128,12 @@ STATIC_URL = '/static/'
 # STATICFILES_DIR =[
 # os.path.join(BASE_DIR, "static")
 # ]
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
+# using below code for hosting website
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
